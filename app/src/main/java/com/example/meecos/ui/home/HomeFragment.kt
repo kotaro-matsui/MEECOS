8f3cb62e5965b1e40e96f54aee23950e3768adb8
Jitsuhara <pikoru323@yahoo.co.jp>
2020-06-25 17:06:41 +0900

    ListViewの作成

--- app/src/main/java/com/example/meecos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/meecos/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.meecos.R
 import com.example.meecos.ui.Base.BaseFragment
@@ -17,5 +19,20 @@ class HomeFragment : BaseFragment() {
     ): View? {
         var view = inflater.inflate(R.layout.fragment_home, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        //リストビューに表示するリストを手動で作成
+        val texts = arrayOf("abc ", "bcd", "cde")
+
+
+        val listView = view.findViewById<ListView>(R.id.today_schedule)
+
+        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
+        val arrayAdapter = ArrayAdapter(requireActivity().applicationContext,
+            android.R.layout.simple_list_item_1, texts)
+
+        listView.adapter = arrayAdapter
     }
 }
