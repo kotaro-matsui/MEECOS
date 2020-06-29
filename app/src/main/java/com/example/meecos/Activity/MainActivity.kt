7eec901039f17284ba822906521172f67a7da8f8
koutarou <matsuikoutarou@kotaroMacBookPro.local>
2020-06-29 16:12:42 +0900

    クラス分け

--- app/src/main/java/com/example/meecos/Activity/MainActivity.kt
+++ app/src/main/java/com/example/meecos/Activity/MainActivity.kt
@@ -31,6 +31,7 @@ import android.speech.SpeechRecognizer
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.meecos.Config.PERMISSIONS_CODE
+import com.example.meecos.Config.SimpleRecognizerListener
 
 class MainActivity : AppCompatActivity(), SimpleRecognizerListener.SimpleRecognizerResponseListener {
 
@@ -178,50 +179,3 @@ class MainActivity : AppCompatActivity(), SimpleRecognizerListener.SimpleRecogni
     }
 }
 
-class SimpleRecognizerListener(private val listener: SimpleRecognizerResponseListener): RecognitionListener {
-
-    interface SimpleRecognizerResponseListener {
-        fun onResultsResponse(speechText: String)
-        fun restart()
-    }
-
-    override fun onReadyForSpeech(p0: Bundle?) { }
-    override fun onRmsChanged(p0: Float) { }
-    override fun onBufferReceived(p0: ByteArray?) { }
-    override fun onPartialResults(p0: Bundle?) { }
-    override fun onEvent(p0: Int, p1: Bundle?) { }
-    override fun onBeginningOfSpeech() { }
-    override fun onEndOfSpeech() { }
-
-    override fun onError(p0: Int) {
-        var error = ""
-        when (p0) {
-            SpeechRecognizer.ERROR_AUDIO -> error = "Audio recording error"
-            SpeechRecognizer.ERROR_CLIENT -> error = "Other client side errors"
-            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> error = "Insufficient permissions"
-            SpeechRecognizer.ERROR_NETWORK -> error = "Network related errors"
-            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> error = "Network operation timed out"
-            SpeechRecognizer.ERROR_NO_MATCH -> error = "No recognition result matched"
-            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> error = "RecognitionService busy"
-            SpeechRecognizer.ERROR_SERVER -> error = "Server sends error status"
-            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> error = "No speech input"
-        }
-        listener.restart()
-    }
-
-    override fun onResults(bundle: Bundle?) {
-        if (bundle == null) {
-            listener.onResultsResponse("")
-            return
-        }
-
-        val key = SpeechRecognizer.RESULTS_RECOGNITION
-        val result = bundle.getStringArrayList(key)
-        val speechText = result?.get(0)
-        if (speechText.isNullOrEmpty()) {
-            listener.onResultsResponse("")
-        } else {
-            listener.onResultsResponse(speechText)
-        }
-    }
-}
