62934b627b650f82f9d637700d26aa9983b482fe
lalabay82 <h_fuchita@bs-ja.co.jp>
2020-07-08 14:52:15 +0900

    マージ後起動確認完了

--- app/src/main/java/com/example/meecos/Model/BaseModel.kt
+++ app/src/main/java/com/example/meecos/Model/BaseModel.kt
@@ -3,22 +3,21 @@ package com.example.meecos.Model
 import io.realm.Realm
 import io.realm.RealmResults
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 open class BaseModel: RealmObject() {
+    @PrimaryKey
+    var sample : String? = null
 
-    class BaseModel : Object() {
-
-        fun <T> delete(target: RealmResults<T>): Boolean {
-            val realm = Realm.getDefaultInstance()
-            return try {
-                realm.executeTransaction {
-                    target.deleteFromRealm(0)
-                }
-                true
-            } catch (e: Exception) {
-                false
+    fun <T> delete(target: RealmResults<T>): Boolean {
+        val realm = Realm.getDefaultInstance()
+        return try {
+            realm.executeTransaction {
+                target.deleteFromRealm(0)
             }
+            true
+        } catch (e: Exception) {
+            false
         }
     }
-
 }
